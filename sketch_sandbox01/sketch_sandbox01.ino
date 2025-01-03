b7055b78d4c212271a4255721d7fabcb704ee034
Kalle Heuwes <karlheinz.heuwes@gmail.com>
2025-01-03 16:17:18 +0100

    Zwischenstand

--- sketch_sandbox01/sketch_sandbox01.ino
+++ sketch_sandbox01/sketch_sandbox01.ino
@@ -14,16 +14,19 @@ void setup() {
 void loop() {
   tft.fillScreen(TFT_BLACK);
   xC = xC + 5; // Center x-coordinate of the arc
+  if(xC >200){
+    xC = 120;
+  }
   int yC = 120; // Center y-coordinate of the arc
   int rad = 100; // Radius of the arc
   int ir = 80; // Radius of the arc
   uint32_t startAngle = 0; // Starting angle of the arc (in degrees)
   uint32_t endAngle = 210; // Ending angle of the arc (in degrees)
-  uint32_t fg_color = TFT_RED; // Color of the arc
+  uint32_t fg_color = TFT_YELLOW; // Color of the arc
   uint32_t bg_color = TFT_BLUE; // Color of the arc
 
   //tft.drawArc(int32_t x, int32_t y, int32_t r, int32_t ir, uint32_t startAngle, uint32_t endAngle, uint32_t fg_color, uint32_t bg_color)
   tft.drawArc(xC, yC, rad, ir, startAngle, endAngle, fg_color, bg_color); // Draw the arc
 
-  delay(1000); // Wait for 1 second
+  delay(100); // Wait for 1 second
 }
